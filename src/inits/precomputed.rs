66db6aaa15413a01ce157f6b5524bcf702ab4f3d
Mikolaj Figurski <miko.f80@gmail.com>
2024-07-19 16:00:55 -0400

    Add precomputed test

--- src/inits/precomputed.rs
+++ src/inits/precomputed.rs
@@ -20,3 +20,25 @@ where
     });
 }
 
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn train_with_precomputed_centroids() {
+    let samples = vec![0.0, 1.0, 10.0, 11.0, 20.0, 21.0];
+    let centroids = vec![0.0, 21.0];
+    let (sample_cnt, sample_dims) = (samples.len(), 1);
+
+    let kmean: KMeans<f32, 8> = KMeans::new(samples, sample_cnt, sample_dims);
+    let result = kmean.kmeans_lloyd(
+      2,
+      200,
+      KMeans::init_precomputed(centroids),
+      &KMeansConfig::default(),
+    );
+
+    assert_eq!(result.centroids, vec![5.5, 20.5]);
+  }
+}
+
